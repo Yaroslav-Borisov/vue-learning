03bd528a64f037c06ac196356ee64dbb81577778
Yaroslav-Borisov <yarpolar@gmail.com>
2024-07-25 23:22:35 +0300

    create simple counter

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,33 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+    <div>
+        <div>
+            <button @click="addLike">like</button>
+            <button @click="addDislike">dislike</button>
+        </div>
+        <div>Количество лайков {{ likes }}</div>
+        <div>Количество дизлайков {{ dislikes }}</div>
+    </div>
 </template>
 
+<script>
+export default {
+    data() {
+        return {
+            likes: 0,
+            dislikes: 5,
+        }
+    },
+    methods: {
+        addLike() {
+            this.likes += 1;
+        },
+        addDislike() {
+            this.dislikes += 1;
+        }
+    }
+}
+</script>
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
 
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>
